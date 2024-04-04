a15aaf1575e595e9b010e79725b7c1edf29d15f6
Whitesoul293 <157641062+Whitesoul293@users.noreply.github.com>
2024-04-04 21:55:56 +0200

    completata la prima traccia e sistemata la seconda git add .
    clear
    +

--- index.php
+++ index.php
@@ -13,24 +13,33 @@ Trasforma quelle variabili in costanti, utilizzando le best practice viste a lez
 Pusha il codice su GitHub con il nome php_01_nome_cognome. Integra anche le altre tracce e poi pusha tutto.
 */
 
-$instalPHP = notChecked; //fatta per memare
+// $instalPHP = notChecked; //fatta per memare
 
 // String
-$name = Alessandro;
+$name = 'Alessandro';
+const NAME = 'Alessandro';
 
 // Integer
 $age = 25;
+const AGE = 25;
 
 // Booleano
 $isStudent = true;
+const ISSTUDENT = true;
 
 // Float
 $heigth = 1.80;
+const HEIGHT = 1.80;
 
 var_dump($name);
 var_dump($age);
 var_dump($isStudent);
 var_dump($heigth);
+
+var_dump(NAME);
+var_dump(AGE);
+var_dump(ISSTUDENT);
+var_dump(HEIGHT);
 
 /*
 TRACCIA 2
@@ -46,13 +55,14 @@ $text8 = "tutto"
 correggi eventuali errori e stampa correttamente a terminale la stringa: “Marco hai sete? Perche' hai bevuto tutto.“
 */
 
-$text1 = "Marco ";
-$text2 = " hai ";
+$text1 = "Marco";
+$text2 = " hai";
 $text3 = "sete";
 $text4 = "?";
-$text5 = "Perchè ";
-$text6 = 'hai ';
-$text7 = 'bevuto ';
+$text5 = "Perchè";
+$text6 = $text2;
+$text7 = "bevuto";
 $text8 = "tutto";
 
-echo $text1 . $text2  . $text3 . $text4 . $text5 . $text6 . $text7 . $text8;
+echo $text1 . " " . $text2 . " " . $text3 . " " . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8 . ".";
+
