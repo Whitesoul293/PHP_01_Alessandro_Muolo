a918569b44605e3c891a3c266e006736dda67449
Whitesoul293 <157641062+Whitesoul293@users.noreply.github.com>
2024-04-04 17:59:22 +0200

    Traccia 1 fatta

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+TRACCIA 1
+Installare PHP
+Definisci 4 variabili:
+Integer
+Float
+String
+Boolean
+A schermo, fai comparire il tipo di dato delle varie variabili.
+Trasforma quelle variabili in costanti, utilizzando le best practice viste a lezione.
+Pusha il codice su GitHub con il nome php_01_nome_cognome. Integra anche le altre tracce e poi pusha tutto.
+*/
+
+$instalPHP = notChecked; //fatta per memare
+
+// String
+$name = Alessandro;
+
+// Integer
+$age = 25;
+
+// Booleano
+$isStudent = true;
+
+// Float
+$heigth = 1.80;
+
+var_dump($name);
+var_dump($age);
+var_dump($isStudent);
+var_dump($heigth);
